78aedcb9c1176001c2d6261247073c29b42ac4d6
zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2024-07-03 06:14:39 +0700

    sync: database

--- database.js
+++ database.js
@@ -1,15 +1,22 @@
 const admin = require("firebase-admin");
 require("dotenv").config();
-var serviceAccount = require("./admin.json");
 
 // Initialize Firebase Admin SDK using environment variables
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key.replace(/\\n/g, "\n"), // Replace escaped newlines
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url,
+  }),
   databaseURL: process.env.databaseURL,
   storageBucket: process.env.storageBucket,
 });
 
 const db = admin.firestore();
-const bucket = admin.storage().bucket();
-
-module.exports = { db, bucket };
